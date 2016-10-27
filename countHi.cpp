4f3cd7fef72328ba2ed8a623903bf4847565ec06
Sabastorm <patrikjusup@gmail.com>
2016-10-27 17:11:02 +0200

    Add files via upload

---
+++ countHi.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstdlib>
+
+int countHi(char [],int);
+
+
+int main(void) {
+	char z[11];
+	scanf("%s", z);
+	int r = countHi(z, 0);
+	printf("%d\n", r);
+	return EXIT_SUCCESS;
+}
+
+int countHi(char s[], int i) {
+	if (s[i] == 0) {
+		return 0;
+	}
+	int hi = s[i] == 'h' || s[i] == 'H' && s[i+1]="I" || s[i+1]="i" ? 1 : 0;
+return hi + countHi(s, i+1+hi);
+}
